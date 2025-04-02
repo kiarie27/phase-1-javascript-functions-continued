b5511ad36f0ae38f910974d23536c729fcc3cdeb
anthony kiarie <akiarie27@gmail.com>
2025-04-02 17:38:17 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+// Define saturdayFun as a function declaration with a default activity
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  // Define mondayWork as a function expression with a default activity
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  // Define wrapAdjective as a higher-order function that returns a function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+
+  // Export the functions for testing purposes
+  module.exports = { saturdayFun, mondayWork, wrapAdjective };
